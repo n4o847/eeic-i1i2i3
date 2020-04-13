ac817e75482f7c6aa261bee941ba900e64473d14
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-04-13 14:40:27 +0900

    本課題2.8

---
+++ 02/read_data.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define N 256
+
+void die(char * s) {
+    perror(s);
+    exit(1);
+}
+
+int main(int argc, char ** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "argument error\n");
+        exit(1);
+    }
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) die("open");
+    unsigned char data[N];
+    int offset = 0;
+    while (1) {
+        int n = read(fd, data, N);
+        if (n == -1) die("read");
+        if (n == 0) break;
+        for (int i = 0; i < n; i++) {
+            printf("%d %d\n", offset + i, data[i]);
+        }
+        offset += n;
+    }
+    close(fd);
+    return 0;
+}
