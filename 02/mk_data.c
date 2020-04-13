41bf80b296c8a7f44586c8d20945ba0b82532fcc
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-04-13 14:30:19 +0900

    準備課題2.5

--- 02/mk_data.c
+++ 02/mk_data.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 #define N 256
 
-int main() {
-    int fd = open("my_data", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+int main(int argc, char ** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "argument error\n");
+        exit(1);
+    }
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     unsigned char data[N];
     for (int i = 0; i < N; i++) {
         data[i] = i;
