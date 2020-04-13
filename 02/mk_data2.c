c2546d4b6eaa99874662a3263e65a1c1d7de39d2
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-04-13 14:32:08 +0900

    準備課題2.6

---
+++ 02/mk_data2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define N 256
+
+void die(char * s) {
+    perror(s);
+    exit(1);
+}
+
+int main(int argc, char ** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "argument error\n");
+        exit(1);
+    }
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1) die("open");
+    unsigned char data[N];
+    for (int i = 0; i < N; i++) {
+        data[i] = i;
+    }
+    write(fd, data, N);
+    close(fd);
+    return 0;
+}
